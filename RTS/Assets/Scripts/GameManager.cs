5d48800591186f9790751b6575a8fd496fa914d5
Geoffrey Bernard <geoffreey.bernard@gmail.com>
2022-11-23 11:29:15 +0100

    MakeGroup merged with optimisation

--- RTS/Assets/Scripts/GameManager.cs
+++ RTS/Assets/Scripts/GameManager.cs
@@ -1,8 +1,7 @@
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Diagnostics;
-using System.Linq;
 
 [RequireComponent(typeof(LocomotionManager))]
 public class GameManager : MonoBehaviour
@@ -13,7 +12,17 @@ public class GameManager : MonoBehaviour
 
     #region Init & Variables
 
-    List<TickedBehaviour> _entities;
+    public class TickedList : KeyedCollection<int, TickedBehaviour>
+    {
+        protected override int GetKeyForItem(TickedBehaviour tickedBehaviour)
+        {
+            return tickedBehaviour.ID;
+        }
+    }
+
+    private TickedList _entities;
+
+    public static TickedList Entities => _instance._entities;
 
     private void Awake()
     {
@@ -24,7 +33,8 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        _entities = new List<TickedBehaviour>();
+        _entities = new TickedList();
+
         _locomotionManager = GetComponent<LocomotionManager>();
     }
 
@@ -87,7 +97,7 @@ public class GameManager : MonoBehaviour
         return new byte[1];
     }
 
-    private bool LineOfSight(Vector2Int start, Vector2Int end)
+    private static bool LineOfSight(Vector2Int start, Vector2Int end)
     {
         int dx = end.x - start.x;
         int dy = end.y - start.y;
@@ -99,10 +109,6 @@ public class GameManager : MonoBehaviour
         int signY = dy > 0 ? 1 : -1;
 
         int ix = 0, iy = 0;
-
-        //Points.Clear();
-
-        //Points.Add(TileMapManager.TilemapCoordsToWorld(start));
 
         while (ix < nx || iy < ny)
         {
@@ -129,8 +135,6 @@ public class GameManager : MonoBehaviour
                 ++iy;
             }
 
-            //Points.Add(TileMapManager.TilemapCoordsToWorld(start));
-
             if (TileMapManager.GetTile(start).isObstacle)
                 return false;
         }
@@ -138,18 +142,67 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
-    private List<List<Character>> MakeGroups(Character[] peons, float maxSqrtMagnitude = 100f)
+    /// <summary>
+    /// Clusterize a list of Character taking into account the obstacles
+    /// </summary>
+    /// <para name="peons">Characters to clusterize</para>
+    /// <para name="maxSqrtMagnitude">The square of the maximum distance to the leader of a group</para>
+    /// <returns>A list of Character list</returns>
+    private List<List<Character>> MakeGroups(Character[] peons, float maxSqrtMagnitude = 100f) 
     {
+        /// <summary>
+        /// Check if all characters can be contained in a square without obstacles
+        /// </summary>
+        bool IsComplexGroupsNeeded(Character[] characters)
+        {
+            if (characters.Length < 2)
+                return false;
+
+            int minX = characters[0].Coords.x;
+            int maxX = characters[0].Coords.x;
+            int minY = characters[0].Coords.y;
+            int maxY = characters[0].Coords.y;
+
+            for (int i = 1; i < characters.Length; ++i)
+            {
+                Vector2Int coords = characters[i].Coords;
+
+                if (coords.x < minX)
+                    minX = coords.x;
+
+                if (coords.x > maxX)
+                    maxX = coords.x;
+
+                if (coords.y < minY)
+                    minY = coords.y;
+
+                if (coords.y > maxY)
+                    maxY = coords.y;
+            }
+
+            return TileMapManager.ObstacleDetection(minX, maxX, minY, maxY);
+        }
+
         List<List<Character>> groups = new List<List<Character>>();
+
+        if (!IsComplexGroupsNeeded(peons))
+        {
+            groups.Add(new List<Character>(peons));
+
+            return groups;
+        }
 
         List<Character> openSet = new List<Character>(peons);
 
+        /// <summary>
+        /// Return the Character closest to the center of gravity of openSet
+        /// </summary>
         int GetLeader()
         {
             Vector2 gravityCenter = Vector2.zero;
 
-            foreach (Peon peon in peons)
-                gravityCenter += (Vector2)peon.transform.position;
+            for (int i = 0; i < peons.Length; ++i)
+                gravityCenter += (Vector2)peons[i].transform.position;
 
             gravityCenter /= peons.Length;
 
@@ -177,20 +230,9 @@ public class GameManager : MonoBehaviour
         {
             int leaderIndex = GetLeader();
 
-            Character leader;
+            Character leader = openSet[leaderIndex];
 
-            if (useGravityCenter)
-            {
-                leader = openSet[leaderIndex];
-
-                openSet.RemoveAt(leaderIndex);
-            }
-            else
-            {
-                leader = openSet[0];
-
-                openSet.RemoveAt(0);
-            }
+            openSet.RemoveAt(leaderIndex);
 
             group = new List<Character>() { leader };
 
@@ -215,254 +257,13 @@ public class GameManager : MonoBehaviour
         return groups;
     }
 
-    public Vector2Int PStart, PEnd;
-    public Vector2Int CStart, CEnd;
-
-    public List<Vector2> Points = new List<Vector2>();
-    public List<List<Character>> Groups;
-    public List<List<Character>> GroupsP;
-    public List<string> Names1;
-    public List<string> Names2;
-    public List<string> Names3;
-    public List<string> Names4;
-    public List<string> Names5;
-    public double lag;
-    public bool lineOfSight;
-    public float distance = 100f;
-    public bool useGravityCenter = true;
-    public bool useGeoffreyAlgorithme = true;
-    public bool activatePaulOpti = true;
-
-    public Vector3 off;
-
-    public static bool RetreiveClustersModified(Character[] characters)
-    { 
-        if (characters.Length < 2)
-            return false;
-
-        int minX = characters[0].Coords.x;
-        int maxX = characters[0].Coords.x;
-        int minY = characters[0].Coords.y;
-        int maxY = characters[0].Coords.y;
-
-        for (int i = 0; i < characters.Length; ++i)
-        {
-            Vector2Int coords = characters[i].Coords;
-
-            if (coords.x < minX)
-                minX = coords.x;
-
-            if (coords.x > maxX)
-                maxX = coords.x;
-
-            if (coords.y < minY)
-                minY = coords.y;
-
-            if (coords.y > maxY)
-                maxY = coords.y;
-        }
-
-        return TileMapManager.ObstacleDetection(minX, maxX, minY, maxY);
-    }
-
-    public static List<List<Character>> RetreiveClusters(Character[] characters)
-    {
-        List<Character> remainingCharacters = new List<Character>(characters);
-        List<List<Character>> clusters = new List<List<Character>>();
-
-        List<Character> XSorted = new List<Character>(characters);
-        List<Character> YSorted = new List<Character>(characters);
-
-        XSorted.Sort((a, b) => a.Coords.x.CompareTo(b.Coords.x));
-        YSorted.Sort((a, b) => a.Coords.y.CompareTo(b.Coords.y));
-
-        int minXindex = XSorted[0].Coords.x;
-        int maxXindex = XSorted[^1].Coords.x;
-        int minYindex = YSorted[0].Coords.y;
-        int maxYindex = YSorted[^1].Coords.y;
-
-        bool[,] clusteringMap;
-        int[] obstaclesCount;
-
-        (clusteringMap, obstaclesCount) = TileMapManager.RetrieveClusteringMap(minXindex, maxXindex, minYindex, maxYindex);
-
-        for (int i = 0; i < obstaclesCount.Length; i++)
-        {
-            if (obstaclesCount[i] != 0)
-                break;
-            else if (i == obstaclesCount.Length - 1)
-            {
-                clusters.Add(remainingCharacters);
-                return clusters;
-            }
-        }
-
-        int currentXmin;
-        int currentXmax;
-        int currentYmin;
-        int currentYmax;
-
-        List<Character> currentCluster = new List<Character>();
-        List<Character> currentXSorted = new List<Character>();
-        List<Character> currentYSorted = new List<Character>();
-
-        Vector2 mean;
-
-        HashSet<Character> edgeCharacters = new HashSet<Character>();
-
-        Character farthestCharacter = null;
-        float farthestDistance;
-
-        while (remainingCharacters.Count > 1)
-        {
-            currentCluster.Clear();
-            currentCluster.AddRange(remainingCharacters);
-
-            mean = Vector2.zero;
-
-            currentXSorted.Clear();
-            currentYSorted.Clear();
-            currentXSorted.AddRange(XSorted);
-            currentYSorted.AddRange(YSorted);
-
-            while (true)
-            {
-                currentXmin = currentXSorted[0].Coords.x - minXindex;
-                currentXmax = currentXSorted[^1].Coords.x - minXindex;
-                currentYmin = currentYSorted[0].Coords.y - minYindex;
-                currentYmax = currentYSorted[^1].Coords.y - minYindex;
-
-                bool obstacleDetected = false;
-
-                for (int y = currentYmin; y <= currentYmax && !obstacleDetected; y++)
-                    if (obstaclesCount[y] != 0)
-                        for(int x = currentXmin; x <= currentXmax && !obstacleDetected; x++)
-                            if(clusteringMap[x, y])
-                                obstacleDetected = true;
-
-                if (!obstacleDetected)
-                    break;
-
-                for (int i = 0; i < currentCluster.Count; ++i)
-                    mean += currentCluster[i].Coords;
-
-                mean /= currentCluster.Count;
-
-                edgeCharacters.Clear();
-
-                edgeCharacters.Add(currentXSorted[0]);
-                edgeCharacters.Add(currentXSorted[^1]);
-                edgeCharacters.Add(currentYSorted[0]);
-                edgeCharacters.Add(currentYSorted[^1]);
-
-                farthestDistance = 0f;
-
-                foreach (Character character in edgeCharacters)
-                {
-                    float distance = (mean - character.Coords).sqrMagnitude;
-                    if (distance > farthestDistance)
-                    {
-                        farthestDistance = distance;
-                        farthestCharacter = character;
-                    }
-                }
-
-                currentXSorted.Remove(farthestCharacter);
-                currentYSorted.Remove(farthestCharacter);
-                currentCluster.Remove(farthestCharacter);
-
-            }
-
-            for(int i = 0; i < currentCluster.Count; ++i) 
-            {
-                Character character = currentCluster[i];
-
-                remainingCharacters.Remove(character);
-                XSorted.Remove(character);
-                YSorted.Remove(character);
-            }
-
-            clusters.Add(new List<Character>(currentCluster));
-        }
-
-        if (remainingCharacters.Count == 1)
-            clusters.Add(remainingCharacters);
-
-        return clusters;
-    }
-
     private void Update()
     {
         Character[] peons = FindObjectsOfType<Character>();
 
-        Stopwatch sw = Stopwatch.StartNew();
+        Color[] colors = new Color[] { new Color(1f, 0f, 0f), new Color(0f, 1f, 0f), new Color(0f, 0f, 1f), new Color(1f, 1f, 0f), new Color(1f, 0f, 1f), new Color(0f, 1f, 1f), new Color(0f, 0f, 0f), new Color(0.5019607843137255f, 0f, 0f), new Color(0f, 0.5019607843137255f, 0f), new Color(0f, 0f, 0.5019607843137255f), new Color(0.5019607843137255f, 0.5019607843137255f, 0f), new Color(0.5019607843137255f, 0f, 0.5019607843137255f), new Color(0f, 0.5019607843137255f, 0.5019607843137255f), new Color(0.5019607843137255f, 0.5019607843137255f, 0.5019607843137255f), new Color(0.7529411764705882f, 0f, 0f), new Color(0f, 0.7529411764705882f, 0f), new Color(0f, 0f, 0.7529411764705882f), new Color(0.7529411764705882f, 0.7529411764705882f, 0f), new Color(0.7529411764705882f, 0f, 0.7529411764705882f), new Color(0f, 0.7529411764705882f, 0.7529411764705882f), new Color(0.7529411764705882f, 0.7529411764705882f, 0.7529411764705882f), new Color(0.25098039215686274f, 0f, 0f), new Color(0f, 0.25098039215686274f, 0f), new Color(0f, 0f, 0.25098039215686274f), new Color(0.25098039215686274f, 0.25098039215686274f, 0f), new Color(0.25098039215686274f, 0f, 0.25098039215686274f), new Color(0f, 0.25098039215686274f, 0.25098039215686274f), new Color(0.25098039215686274f, 0.25098039215686274f, 0.25098039215686274f), new Color(0.12549019607843137f, 0f, 0f), new Color(0f, 0.12549019607843137f, 0f), new Color(0f, 0f, 0.12549019607843137f), new Color(0.12549019607843137f, 0.12549019607843137f, 0f), new Color(0.12549019607843137f, 0f, 0.12549019607843137f), new Color(0f, 0.12549019607843137f, 0.12549019607843137f), new Color(0.12549019607843137f, 0.12549019607843137f, 0.12549019607843137f), new Color(0.3764705882352941f, 0f, 0f), new Color(0f, 0.3764705882352941f, 0f), new Color(0f, 0f, 0.3764705882352941f), new Color(0.3764705882352941f, 0.3764705882352941f, 0f), new Color(0.3764705882352941f, 0f, 0.3764705882352941f), new Color(0f, 0.3764705882352941f, 0.3764705882352941f), new Color(0.3764705882352941f, 0.3764705882352941f, 0.3764705882352941f), new Color(0.6274509803921569f, 0f, 0f), new Color(0f, 0.6274509803921569f, 0f), new Color(0f, 0f, 0.6274509803921569f), new Color(0.6274509803921569f, 0.6274509803921569f, 0f), new Color(0.6274509803921569f, 0f, 0.6274509803921569f), new Color(0f, 0.6274509803921569f, 0.6274509803921569f), new Color(0.6274509803921569f, 0.6274509803921569f, 0.6274509803921569f), new Color(0.8784313725490196f, 0f, 0f), new Color(0f, 0.8784313725490196f, 0f), new Color(0f, 0f, 0.8784313725490196f), new Color(0.8784313725490196f, 0.8784313725490196f, 0f), new Color(0.8784313725490196f, 0f, 0.8784313725490196f), new Color(0f, 0.8784313725490196f, 0.8784313725490196f), new Color(0.8784313725490196f, 0.8784313725490196f, 0.8784313725490196f) };
 
-        //LineOfSight(PStart, PEnd); Groups = new List<List<Peon>>();
-
-        if (activatePaulOpti)
-        {
-            if (RetreiveClustersModified(peons))
-                Groups = MakeGroups(peons, distance);
-            else
-                Groups = new List<List<Character>>() { peons.ToList() };
-        }
-        else
-            Groups = MakeGroups(peons, distance);
-
-        sw.Stop();
-
-        lag = sw.Elapsed.TotalMilliseconds * 1000.0;
-
-        UnityEngine.Debug.Log(lag);
-
-        if (useGeoffreyAlgorithme)
-        {
-            for (int i = 0; i < Groups.Count; ++i)
-            {
-                List<string> names = new List<string>(Groups[i].Count);
-
-                for (int j = 0; j < Groups[i].Count; ++j)
-                    names.Add(Groups[i][j].name);
-
-                if (i == 0)
-                    Names1 = names;
-                else if (i == 1)
-                    Names2 = names;
-                else if (i == 2)
-                    Names3 = names;
-                else if (i == 3)
-                    Names4 = names;
-                else if (i == 4)
-                    Names5 = names;
-            }
-        }
-        else
-        {
-            Names1.Clear();
-            Names2.Clear();
-            Names3.Clear();
-            Names4.Clear();
-            Names5.Clear();
-
-            for (int i = 0; i < GroupsP.Count; ++i)
-            {
-                List<string> names = new List<string>(GroupsP[i].Count);
-
-                for (int j = 0; j < GroupsP[i].Count; ++j)
-                    names.Add(GroupsP[i][j].name);
-
-                if (i == 0)
-                    Names1 = names;
-                else if (i == 1)
-                    Names2 = names;
-                else if (i == 2)
-                    Names3 = names;
-                else if (i == 3)
-                    Names4 = names;
-                else if (i == 4)
-                    Names5 = names;
-            }
-        }
+        List<List<Character>> groups = MakeGroups(peons);
     }
 
     private void OnDrawGizmos()
@@ -470,44 +271,19 @@ public class GameManager : MonoBehaviour
         if (!Application.isPlaying)
             return;
 
-        List<Color> EcolorList = new List<Color>() {
-            Color.red,
-            Color.green,
-            Color.yellow,
-            Color.magenta,
-            new Color(255F, 0F, 255F),
-            new Color(0F, 255F, 255F),
-            new Color(255F, 255F, 0F),
-            new Color(128F, 0F, 128F),
-            new Color(128F, 0F, 0F)
-        };
+        CharacterManager.SelectedCharacters();
 
         // Debug Groups
 
-        if (useGeoffreyAlgorithme)
+        /*for (int i = 0; i < Groups.Count; ++i)
         {
-            for (int i = 0; i < Groups.Count; ++i)
-            {
-                Gizmos.color = EcolorList[i];
+            Gizmos.color = EcolorList[i];
 
-                foreach (Peon peon in Groups[i])
-                {
-                    Gizmos.DrawWireSphere(peon.transform.position, TileMapManager.TileSize);
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < GroupsP.Count; ++i)
+            foreach (Peon peon in Groups[i])
             {
-                Gizmos.color = EcolorList[i];
-
-                foreach (Peon peon in GroupsP[i])
-                {
-                    Gizmos.DrawWireSphere(peon.transform.position, TileMapManager.TileSize);
-                }
+                Gizmos.DrawWireSphere(peon.transform.position, TileMapManager.TileSize);
             }
-        }
+        }*/
 
         // Debug Line Of Sight
 
