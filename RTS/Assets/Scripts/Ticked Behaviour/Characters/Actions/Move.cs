bfbbd442a868f08aa00216cf67a9eb63754016c2
Haenithyll <71867627+Haenithyll@users.noreply.github.com>
2022-11-30 09:17:02 +0100

    Local Avoidance v1

--- RTS/Assets/Scripts/Ticked Behaviour/Characters/Actions/Move.cs
+++ RTS/Assets/Scripts/Ticked Behaviour/Characters/Actions/Move.cs
@@ -12,14 +12,22 @@ public class Move : Action
 
     public override bool Perform()
     {
+        if(_positions.Count == 1 && (_positions[0] - (Vector2)_character.transform.position).sqrMagnitude < .2f)
+            return true;
+
         if (CharacterManager.Move(_character, _positions[0]))
             _positions.RemoveAt(0);
 
         return _positions.Count == 0;
     }
 
-    public Vector2 currentWayPoint()
+    public Vector2 CurrentWayPoint()
     {
         return _positions[0];
+    }
+
+    public Vector2 FinalWayPoint()
+    {
+        return _positions[^1];
     }
 }
